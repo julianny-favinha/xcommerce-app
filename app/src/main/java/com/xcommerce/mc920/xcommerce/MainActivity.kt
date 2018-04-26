19df28bb32c391369fe0260226c8caf7a557cacb
julianny-favinha <julianny.favinha@gmail.com>
2018-04-26 17:40:46 -0300

    Testando novos ícones na navigation drawer. Já está com as tabs corretas segundo NinjaMock

--- app/src/main/java/com/xcommerce/mc920/xcommerce/MainActivity.kt
+++ app/src/main/java/com/xcommerce/mc920/xcommerce/MainActivity.kt
@@ -67,22 +67,10 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         // Handle navigation view item clicks here.
         when (item.itemId) {
-            R.id.nav_camera -> {
-                // Handle the camera action
-            }
-            R.id.nav_gallery -> {
+            R.id.nav_meuspedidos -> {
 
             }
-            R.id.nav_slideshow -> {
-
-            }
-            R.id.nav_manage -> {
-
-            }
-            R.id.nav_share -> {
-
-            }
-            R.id.nav_send -> {
+            R.id.nav_sac -> {
 
             }
         }
