ffee0bdbbe10dc373b9c5db5bd8525b3470e4e90
Igor Hitzschky <igor.lema@ingressorapido.com.br>
2018-04-29 21:16:56 -0300

    add async task

---
+++ app/src/main/java/com/xcommerce/mc920/xcommerce/home/highlights/HighlightsFetchTask.kt
@@ -0,0 +1,42 @@
+package com.xcommerce.mc920.xcommerce.home.highlights
+
+import android.os.AsyncTask
+import com.xcommerce.mc920.xcommerce.model.Highlights
+import com.xcommerce.mc920.xcommerce.model.Product
+
+
+class HighlightsFetchTask(private var container: HighlightsFragment?) : AsyncTask<String, Void, Highlights>() {
+
+    override fun doInBackground(vararg p0: String?): Highlights {
+        try {
+            Thread.sleep(3000)
+            return Highlights(listOf(Product(id=0, name="Sofá", brand="Marca de um sofá", price=100L, category = "Eletrodomésticos", description = "Sofá marrom de 2 lugares que abre para deitar.", imageUrl="mock_sofa"),
+                    Product(id=1, name="Sofá", brand="Marca de um sofá", price=100L, category = "Eletrodomésticos", description = "Sofá marrom de 2 lugares que abre para deitar.", imageUrl="mock_sofa"),
+                    Product(id=2, name="Sofá", brand="Marca de um sofá", price=100L, category = "Eletrodomésticos", description = "Sofá marrom de 2 lugares que abre para deitar.", imageUrl="mock_sofa"),
+                    Product(id=3, name="Sofá", brand="Marca de um sofá", price=100L, category = "Eletrodomésticos", description = "Sofá marrom de 2 lugares que abre para deitar.", imageUrl="mock_sofa"),
+                    Product(id=4, name="Sofá", brand="Marca de um sofá", price=100L, category = "Eletrodomésticos", description = "Sofá marrom de 2 lugares que abre para deitar.", imageUrl="mock_sofa"),
+                    Product(id=5, name="Sofá", brand="Marca de um sofá", price=100L, category = "Eletrodomésticos", description = "Sofá marrom de 2 lugares que abre para deitar.", imageUrl="mock_sofa"),
+                    Product(id=6, name="Sofá", brand="Marca de um sofá", price=100L, category = "Eletrodomésticos", description = "Sofá marrom de 2 lugares que abre para deitar.", imageUrl="mock_sofa"),
+                    Product(id=7, name="Sofá", brand="Marca de um sofá", price=100L, category = "Eletrodomésticos", description = "Sofá marrom de 2 lugares que abre para deitar.", imageUrl="mock_sofa"),
+                    Product(id=8, name="Sofá", brand="Marca de um sofá", price=100L, category = "Eletrodomésticos", description = "Sofá marrom de 2 lugares que abre para deitar.", imageUrl="mock_sofa"),
+                    Product(id=9, name="Sofá", brand="Marca de um sofá", price=100L, category = "Eletrodomésticos", description = "Sofá marrom de 2 lugares que abre para deitar.", imageUrl="mock_sofa"),
+                    Product(id=10, name="Sofá", brand="Marca de um sofá", price=100L, category = "Eletrodomésticos", description = "Sofá marrom de 2 lugares que abre para deitar.", imageUrl="mock_sofa"),
+                    Product(id=11, name="Sofá", brand="Marca de um sofá", price=100L, category = "Eletrodomésticos", description = "Sofá marrom de 2 lugares que abre para deitar.", imageUrl="mock_sofa"),
+                    Product(id=12, name="Sofá", brand="Marca de um sofá", price=100L, category = "Eletrodomésticos", description = "Sofá marrom de 2 lugares que abre para deitar.", imageUrl="mock_sofa")))
+        } catch (e: Exception) {
+            return Highlights(emptyList())
+        }
+    }
+
+    override fun onPreExecute() {
+        super.onPreExecute()
+        container?.showProgressBar()
+    }
+
+    override fun onPostExecute(result: Highlights?) {
+        super.onPostExecute(result)
+        if(result != null) container?.populateResult(result.products)
+        container?.hideProgressBar()
+        this.container = null
+    }
+}
--- app/src/main/java/com/xcommerce/mc920/xcommerce/home/highlights/HighlightsFragment.kt
+++ app/src/main/java/com/xcommerce/mc920/xcommerce/home/highlights/HighlightsFragment.kt
@@ -1,5 +1,6 @@
 package com.xcommerce.mc920.xcommerce.home.highlights
 
+import android.os.AsyncTask
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.widget.LinearLayoutManager
@@ -8,40 +9,62 @@ import android.view.View
 import android.view.ViewGroup
 import com.xcommerce.mc920.xcommerce.R
 import com.xcommerce.mc920.xcommerce.home.highlights.recycler_view.ProductsAdapter
+import com.xcommerce.mc920.xcommerce.model.Highlights
 import com.xcommerce.mc920.xcommerce.model.Product
 import kotlinx.android.synthetic.main.fragment_highlights.*
 
 class HighlightsFragment : Fragment() {
 
+    private var highlights = emptyList<Product>()
+    private var task: HighlightsFetchTask? = null
+
+
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        return inflater?.inflate(R.layout.fragment_highlights, container, false)
+        val inflate = inflater?.inflate(R.layout.fragment_highlights, container, false)
 
+        recycler_view.layoutManager = LinearLayoutManager(this.context)
+        recycler_view.adapter = ProductsAdapter(highlights, this)
 
+        task = HighlightsFetchTask(this)
+        task?.execute("bla")
+
+        return inflate
     }
 
     override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+
+        if(isTaskRunning(task)){
+            showProgressBar()
+        } else {
+            hideProgressBar()
+        }
+
+        if(highlights.isNotEmpty()) {
+            populateResult(highlights)
+        }
+
         super.onViewCreated(view, savedInstanceState)
+    }
 
-        // TODO: Get data from backend
-        val products = listOf(Product(id=0, name="Sofá", brand="Marca de um sofá", price=100L, category = "Eletrodomésticos", description = "Sofá marrom de 2 lugares que abre para deitar.", imageUrl="mock_sofa"),
-                Product(id=1, name="Sofá", brand="Marca de um sofá", price=100L, category = "Eletrodomésticos", description = "Sofá marrom de 2 lugares que abre para deitar.", imageUrl="mock_sofa"),
-                Product(id=2, name="Sofá", brand="Marca de um sofá", price=100L, category = "Eletrodomésticos", description = "Sofá marrom de 2 lugares que abre para deitar.", imageUrl="mock_sofa"),
-                Product(id=3, name="Sofá", brand="Marca de um sofá", price=100L, category = "Eletrodomésticos", description = "Sofá marrom de 2 lugares que abre para deitar.", imageUrl="mock_sofa"),
-                Product(id=4, name="Sofá", brand="Marca de um sofá", price=100L, category = "Eletrodomésticos", description = "Sofá marrom de 2 lugares que abre para deitar.", imageUrl="mock_sofa"),
-                Product(id=5, name="Sofá", brand="Marca de um sofá", price=100L, category = "Eletrodomésticos", description = "Sofá marrom de 2 lugares que abre para deitar.", imageUrl="mock_sofa"),
-                Product(id=6, name="Sofá", brand="Marca de um sofá", price=100L, category = "Eletrodomésticos", description = "Sofá marrom de 2 lugares que abre para deitar.", imageUrl="mock_sofa"),
-                Product(id=7, name="Sofá", brand="Marca de um sofá", price=100L, category = "Eletrodomésticos", description = "Sofá marrom de 2 lugares que abre para deitar.", imageUrl="mock_sofa"),
-                Product(id=8, name="Sofá", brand="Marca de um sofá", price=100L, category = "Eletrodomésticos", description = "Sofá marrom de 2 lugares que abre para deitar.", imageUrl="mock_sofa"),
-                Product(id=9, name="Sofá", brand="Marca de um sofá", price=100L, category = "Eletrodomésticos", description = "Sofá marrom de 2 lugares que abre para deitar.", imageUrl="mock_sofa"),
-                Product(id=10, name="Sofá", brand="Marca de um sofá", price=100L, category = "Eletrodomésticos", description = "Sofá marrom de 2 lugares que abre para deitar.", imageUrl="mock_sofa"),
-                Product(id=11, name="Sofá", brand="Marca de um sofá", price=100L, category = "Eletrodomésticos", description = "Sofá marrom de 2 lugares que abre para deitar.", imageUrl="mock_sofa"),
-                Product(id=12, name="Sofá", brand="Marca de um sofá", price=100L, category = "Eletrodomésticos", description = "Sofá marrom de 2 lugares que abre para deitar.", imageUrl="mock_sofa"))
+    fun showProgressBar(){
+        progressbar.visibility = View.VISIBLE
+        recycler_view.visibility = View.GONE
+    }
 
-        recycler_view.layoutManager = LinearLayoutManager(this.context)
-        recycler_view.adapter = ProductsAdapter(products, this)
+    fun hideProgressBar(){
+        progressbar.visibility = View.GONE
+        recycler_view.visibility = View.VISIBLE
+
+    }
+
+    fun populateResult(highlights: List<Product>) {
+        this.highlights = highlights
         recycler_view.adapter.notifyDataSetChanged()
+    }
 
+    private fun isTaskRunning(task: HighlightsFetchTask?): Boolean {
+        return task?.status != AsyncTask.Status.FINISHED
     }
 
 }
