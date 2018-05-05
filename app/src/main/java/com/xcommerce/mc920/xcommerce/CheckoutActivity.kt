e818d604e07239a3d87febf3677520c5ae9187d4
julianny-favinha <julianny.favinha@gmail.com>
2018-05-05 19:07:42 -0300

    Ids, clique no radio button muda estado do botão de add info credit card

--- app/src/main/java/com/xcommerce/mc920/xcommerce/CheckoutActivity.kt
+++ app/src/main/java/com/xcommerce/mc920/xcommerce/CheckoutActivity.kt
@@ -3,8 +3,11 @@ package com.xcommerce.mc920.xcommerce
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
+import android.widget.Button
+import android.widget.RadioGroup
 
 import kotlinx.android.synthetic.main.activity_checkout.*
+import kotlinx.android.synthetic.main.content_checkout.*
 
 class CheckoutActivity : AppCompatActivity() {
 
@@ -12,6 +15,21 @@ class CheckoutActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_checkout)
         setSupportActionBar(toolbar)
+
+        checkout_radio_payment.setOnCheckedChangeListener{ _, optionId ->
+            val button = findViewById<Button>(R.id.checkout_add_info_credit_card)
+
+            when (optionId) {
+                R.id.checkout_radio_button_credit_card -> {
+                    button.isEnabled = true
+                }
+
+                R.id.checkout_radio_button_boleto -> {
+                    button.isEnabled = false
+                }
+            }
+        }
+
     }
 
 }
