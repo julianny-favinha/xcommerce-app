e604b42f75f31854984ff7568a220dd95842916c
julianny-favinha <julianny.favinha@gmail.com>
2018-05-05 13:40:24 -0300

    Arquivos iniciais para tela de info do cartao de credito

---
+++ app/src/main/java/com/xcommerce/mc920/xcommerce/InfoCreditCartActivity.kt
@@ -0,0 +1,22 @@
+package com.xcommerce.mc920.xcommerce
+
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity
+
+import kotlinx.android.synthetic.main.activity_info_credit_cart.*
+
+class InfoCreditCartActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_info_credit_cart)
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
+        }
+    }
+
+}
