a536c02bc35b886af0c21e353c2bf08ad4dca2a3
julianny-favinha <julianny.favinha@gmail.com>
2018-05-05 19:50:59 -0300

    Ids, alguns cliques

--- app/src/main/java/com/xcommerce/mc920/xcommerce/CheckoutActivity.kt
+++ app/src/main/java/com/xcommerce/mc920/xcommerce/CheckoutActivity.kt
@@ -1,5 +1,6 @@
 package com.xcommerce.mc920.xcommerce
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
@@ -17,7 +18,7 @@ class CheckoutActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
 
         checkout_radio_payment.setOnCheckedChangeListener{ _, optionId ->
-            val button = findViewById<Button>(R.id.checkout_add_info_credit_card)
+            val button = findViewById<Button>(R.id.checkout_button_add_info_card)
 
             when (optionId) {
                 R.id.checkout_radio_button_credit_card -> {
@@ -30,6 +31,16 @@ class CheckoutActivity : AppCompatActivity() {
             }
         }
 
+        // finish
+        checkout_button.setOnClickListener{
+
+        }
+
+        // add credit card info
+        checkout_button_add_info_card.setOnClickListener {
+            val intent = Intent(this, InfoCreditCardActivity::class.java)
+            startActivity(intent)
+        }
     }
 
 }
