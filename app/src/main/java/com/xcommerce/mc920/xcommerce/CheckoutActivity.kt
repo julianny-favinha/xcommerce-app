7b1108e8d4c6e43a0d90acfb30ef545b8829c74f
julianny-favinha <julianny.favinha@gmail.com>
2018-05-05 22:36:23 -0300

    Botão para calcular frete

--- app/src/main/java/com/xcommerce/mc920/xcommerce/CheckoutActivity.kt
+++ app/src/main/java/com/xcommerce/mc920/xcommerce/CheckoutActivity.kt
@@ -3,8 +3,10 @@ package com.xcommerce.mc920.xcommerce
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
 import android.widget.Button
 import kotlinx.android.synthetic.main.activity_checkout.*
+import kotlinx.android.synthetic.main.content_address.*
 import kotlinx.android.synthetic.main.content_checkout.*
 
 class CheckoutActivity : AppCompatActivity() {
@@ -32,6 +34,12 @@ class CheckoutActivity : AppCompatActivity() {
         checkout_button_add_address.setOnClickListener {
             val intent = Intent(this, AddressActivity::class.java)
             startActivity(intent)
+        }
+
+        // calculate shipping rates
+        checkout_button_calculate_shipping.setOnClickListener {
+            checkout_price_pac.visibility = View.VISIBLE
+            checkout_price_sedex.visibility = View.VISIBLE
         }
 
         // add credit card info
