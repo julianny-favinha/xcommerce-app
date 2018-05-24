f6d61100953fd57706f3c36e209fd7df49ce2971
Julianny Favinha <julianny.favinha@gmail.com>
2018-05-24 20:55:13 -0300

    WIP

--- app/src/main/java/com/xcommerce/mc920/xcommerce/CheckoutActivity.kt
+++ app/src/main/java/com/xcommerce/mc920/xcommerce/CheckoutActivity.kt
@@ -1,15 +1,20 @@
 package com.xcommerce.mc920.xcommerce
 
 import android.content.Intent
+import android.os.AsyncTask
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.view.View
 import android.widget.Button
+import com.xcommerce.mc920.xcommerce.checkout.ShipmentPriceFetchTask
+import com.xcommerce.mc920.xcommerce.model.ShipmentIn
 import kotlinx.android.synthetic.main.activity_checkout.*
-import kotlinx.android.synthetic.main.content_address.*
 import kotlinx.android.synthetic.main.content_checkout.*
 
 class CheckoutActivity : AppCompatActivity() {
+    var prices: Map<String, Int> = emptyMap()
+    private var task: ShipmentPriceFetchTask? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,8 +43,10 @@ class CheckoutActivity : AppCompatActivity() {
 
         // calculate shipping rates
         checkout_button_calculate_shipping.setOnClickListener {
-            checkout_price_pac.visibility = View.VISIBLE
-            checkout_price_sedex.visibility = View.VISIBLE
+            task = ShipmentPriceFetchTask(this)
+            //val p = ShipmentIn("PAC", "17020510", "13083730", 1,"Caixa",1, 1,1)
+            //task?.execute(p)
+            task?.execute()
         }
 
         // add credit card info
@@ -52,6 +59,29 @@ class CheckoutActivity : AppCompatActivity() {
         checkout_button.setOnClickListener{
 
         }
+    }
+
+//    fun showProgressBar(){
+//        progressbar.visibility = View.VISIBLE
+//        recycler_view_categories.visibility = View.GONE
+//    }
+//
+//    fun hideProgressBar(){
+//        progressbar.visibility = View.GONE
+//        recycler_view_categories.visibility = View.VISIBLE
+//    }
+
+    fun populateResult(prices: Map<String, Int>) {
+        checkout_price_pac.text = prices["pac"].toString()
+        checkout_price_sedex.text = prices["sedex"].toString()
+        checkout_price_pac.visibility = View.VISIBLE
+        checkout_price_sedex.visibility = View.VISIBLE
+
+        Log.d("Calculate shipment", prices.toString())
+    }
+
+    private fun isTaskRunning(task: ShipmentPriceFetchTask?): Boolean {
+        return task?.status != AsyncTask.Status.FINISHED
     }
 
 }
---
+++ app/src/main/java/com/xcommerce/mc920/xcommerce/model/Logistic.kt
@@ -0,0 +1,7 @@
+package com.xcommerce.mc920.xcommerce.model
+
+data class ShipmentOut(val prices: Map<String, Int>)
+
+data class ShipmentIn(val typeShip: String, val cepFrom: String, val cepDst: String, val weight: Long, val typePack: String, val length: Long, val height: Long, val width: Long)
+
+data class OrderIn(val product: Product, val cepDst: String)
