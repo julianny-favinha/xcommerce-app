f6aea12e07b160e242202a72a58156ee89543886
Yuri Delgado <ydelgado@localhost.localdomain>
2018-06-15 00:38:29 -0300

    merge my_orders

--- app/src/main/java/com/xcommerce/mc920/xcommerce/model/CartItem.kt
+++ app/src/main/java/com/xcommerce/mc920/xcommerce/model/CartItem.kt
@@ -1,9 +1,5 @@
 package com.xcommerce.mc920.xcommerce.model
 
-<<<<<<< HEAD
-data class CartItem(val product: Product, var quantity: Int)
-=======
 import java.io.Serializable
 
-data class CartItem(val product: Product, val quantity: Int) : Serializable
->>>>>>> my_orders
+data class CartItem(val product: Product, var quantity: Int) : Serializable
